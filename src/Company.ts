0452ce8f6f1fccde939f6f3cd7c8e5a4c8509654
mamdouh <douharamadan22@gmail.com>
2021-02-23 23:01:32 +0200

    first commit

---
+++ src/Company.ts
@@ -0,0 +1,28 @@
+import faker from "faker";
+
+export class Company {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `
+      <div>
+        <h3> Company name: ${this.companyName}</h3>
+        <h5> catchPhrase: ${this.catchPhrase}</h5>
+      <div>
+    `;
+  }
+}
